ee1251c2d9203193691b60f5228a0d67f552e07c
kamil <selwak@gmail.com>
2011-08-10 14:58:20 +0100

    login required added

--- commitlog/views.py
+++ commitlog/views.py
@@ -1,11 +1,15 @@
 # Create your views here.
 from git import *
-
+from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
 
-from commitlog.settings import REPO_DIR, REPO_BRANCH, REPO_ITEMS_IN_PAGE
+from commitlog.settings import REPO_DIR, REPO_BRANCH, REPO_ITEMS_IN_PAGE, REPO_RESTRICT_VIEW
 
 def log_view(request, page=0):
     repo = Repo(REPO_DIR)
-    commits = repo.iter_commits(REPO_BRANCH, max_count=1010, skip=REPO_ITEMS_IN_PAGE)
+    start = page + REPO_ITEMS_IN_PAGE
+    commits = repo.iter_commits(REPO_BRANCH, max_count=start, skip=start + REPO_ITEMS_IN_PAGE)
     return TemplateResponse(request, 'commitlog/commitlog.html', {'commits': commits})
+
+if REPO_RESTRICT_VIEW:
+	log_view = login_required(log_view)
