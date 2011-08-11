47d29e201b037bd0b8f3182ee525457869651c9b
kamil <selwak@gmail.com>
2011-08-11 14:52:37 +0100

    edit file support added

--- commitlog/forms.py
+++ commitlog/forms.py
@@ -1,3 +1,10 @@
 from django import forms
 
 # place form definition here
+
+class FileEditForm(forms.Form):
+	"""docstring for FileEditForm"""
+	
+	file_source = forms.CharField( widget=forms.Textarea )
+	message = forms.CharField( widget=forms.Textarea )
+		
