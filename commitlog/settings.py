75ee9331b4e9e9c3d8288d93b5c46f2605727afa
k1000 <selwak@gmail.com>
2011-08-12 00:33:12 +0100

    tree added

--- commitlog/settings.py
+++ commitlog/settings.py
@@ -6,4 +6,7 @@ REPO_DIR = getattr(settings, "REPO_DIR", "")
 REPO_BRANCH = getattr(settings, "REPO_BRANCH", "master")
 REPO_RESTRICT_VIEW = getattr(settings, "REPO_RESTRICT_VIEW", True)
 REPO_ITEMS_IN_PAGE = getattr(settings, "REPO_VIEW_IN_PAGE", 10)
+FILE_BLACK_LIST = getattr(settings,"FILE_BLACK_LIST", 
+	("settings.py",) 
+)
  
--- commitlog/urls.py
+++ commitlog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 # place app url patterns here
 urlpatterns = patterns('',
 	url(r'^(?P<branch>[a-z0-9\-_]+)/edit/(?P<path>[a-z0-9\-_\.\/]+)$', "commitlog.views.edit_file", name='commitlog-edit-file'),
+    url(r'^(?P<branch>[a-z0-9\-_]+)/tree/(?P<path>[a-z0-9\-_\.\/]*)$', "commitlog.views.tree_view", name='commitlog-tree-view'),
     url(r'^(?P<branch>[a-z0-9\-_]+)/$', "commitlog.views.log_view", name='commitlog-branch'),
 )
