173a172099dc3532c3a8260d19ff02c349bb8e67
kamil <selwak@gmail.com>
2011-08-11 10:37:24 +0100

    paging in views

--- commitlog/urls.py
+++ commitlog/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 
 # place app url patterns here
-urlpatterns = patterns('commitlog.views',
-    url(r'^(?P<branch>[-\w]+)/$', "log_view", name='commitlog-branch'),
-    #url(r'^/$', login_required( LookingFormView.as_view() ), name='commitlog-branch'),
+urlpatterns = patterns('',
+    url(r'^(?P<branch>[a-z0-9\-_]+)/$', "commitlog.views.log_view", name='commitlog-branch'),
 )
