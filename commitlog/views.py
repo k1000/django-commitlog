e0d44763523cbaf4b5c6d5a9339dae5046e2ddca
kamil <selwak@gmail.com>
2011-08-12 14:45:29 +0100

    tree view added

--- commitlog/views.py
+++ commitlog/views.py
@@ -1,11 +1,12 @@
 # Create your views here.
+import codecs
 from git import *
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
 from django.contrib import messages
 
 from commitlog.settings import REPO_DIR, REPO_BRANCH, REPO_ITEMS_IN_PAGE, REPO_RESTRICT_VIEW, FILE_BLACK_LIST
-from commitlog.forms import FileEditForm
+from commitlog.forms import TextFileEditForm, FileEditForm
 
 def log_view(request, branch=REPO_BRANCH):
     page = int(request.GET.get("page", 0))
@@ -30,14 +31,23 @@ if REPO_RESTRICT_VIEW:
     log_view = login_required(log_view)
 
 
+
+
+
 @login_required
 def tree_view(request, branch=REPO_BRANCH, path=None ):
 
     repo = Repo(REPO_DIR)
+    tree = repo.tree()
+    if path:
+        if path[-1:] == "/":
+            path = path[:-1]
+        tree = tree[path]
 
     context = dict(
         branch_name = branch,
-        tree = repo.tree(),
+        tree = tree.list_traverse(depth = 1),
+        dir_path = path.split("/"),
     )
 
     return TemplateResponse( 
@@ -45,11 +55,47 @@ def tree_view(request, branch=REPO_BRANCH, path=None ):
         'commitlog/admin_view_tree.html', 
         context)
 
+def guess_file_type(path):
+    import os
+    basename, extension = os.path.splitext(path)
+    ext = {
+        ".py" : {"type":"python", "mime":"text"},
+        ".html" : {"type":"xml", "mime":"text"},
+        ".css" : {"type":"css", "mime":"text"},
+        ".js" : {"type":"javascript", "mime":"text"},
+        ".json" : {"type":"javascript", "mime":"text"},
+        ".svg" : {"type":"svg", "mime":"text"},
+        ".txt" : {"type":"text", "mime":"text"},
+        ".png" : {"type":"css", "mime":"image"},
+        ".jpg" : {"type":"jpg", "mime":"image"},
+        ".gif" : {"type":"gif", "mime":"image"},
+        ".pdf" : {"type":"pdf", "mime":"doc"},
+        ".doc" : {"type":"doc", "mime":"doc"},
+    }
+    if extension in ext:
+        return ext[extension]
+    else:
+        return {"type":"text", "mime":"text"}
+
+def handle_uploaded_file( path, f):
+    destination = open(path, 'wb+')
+    for chunk in f.chunks():
+        destination.write(chunk)
+    destination.close()
+
+def write_file( file_path, file_source):
+    f = codecs.open(file_path, encoding='utf-8', mode='w')
+    try:
+        f.write(file_source)
+    except Exception, e:
+        raise
+    finally:
+        return True
+
 
 @login_required
 def edit_file(request, branch=REPO_BRANCH, path=None ):
     import os
-    import codecs
 
     result_msg = ""
     file_source = ""
@@ -57,41 +103,67 @@ def edit_file(request, branch=REPO_BRANCH, path=None ):
 
     if path in FILE_BLACK_LIST:
         pass
+    
+    tree = repo.tree()
+    if path[-1:] == "/":
+        path = path[:-1]
+    tree = tree[path]
 
-    file_path = os.path.join( repo.working_dir, path ) #!!! sanitize path
+    file_path = tree.abspath
+
+    file_mime = tree.mime_type
+    file_mime_type = file_mime.split("/")[0]
+
+    file_type = guess_file_type(path)
+
+    if file_mime_type == "text":
+        form_class = TextFileEditForm
+    else:
+        form_class = FileEditForm
     
     if request.method == 'POST':
-        
-        form = FileEditForm( request.POST )
+        form = form_class( request.POST, request.FILES )
+
         if form.is_valid():
             #f = open(file_path, "rw")
-            f = codecs.open(file_path, encoding='utf-8', mode='w')
-            file_source = form.cleaned_data["file_source"]
+            if file_mime_type == "text":
+                file_source = form.cleaned_data["file_source"]
+                write_file(file_path, file_source )
+            else:
+                handle_uploaded_file(file_path, request.FILES['file_source'])
+
+            index = repo.index
+            message = form.cleaned_data["message"]
             try:
-                f.write(file_source)
-            except Exception, e:
+                #git = repo.git
+                #commit_result = git.commit("-m", """%s""" % message)
+                
+                commit_result = index.commit("""%s""" % message)
+            except GitCommandError as er:
+                result_msg = u"There been problem applying you commit. %s" % er
                 raise
-            finally:
-                message = form.cleaned_data["message"]
-                #index = repo.index
-                #new_commit = index.commit("my commit message")
-                git = repo.git
-                commit_result = git.commit("-m", """%s""" % message)
+            else:
                 result_msg = u"Commit has been executed. <br/>%s" % commit_result
         else:
             result_msg = "There were problems with making commit"
 
     else:
-        f = codecs.open(file_path, encoding='utf-8',)
-        file_source = f.read
+        
+        if file_mime_type == "text":
+            f = codecs.open(file_path, encoding='utf-8',)
+            file_source = f.read
+        else:
+            file_source = file_path
 
-        form = FileEditForm( initial={"file_source":file_source} )
-    #
+        form = form_class( initial={"file_source":file_source} )
+
     context = dict(
         form= form,
         file_source = file_source,
+        file_type = file_type,
         result_msg = result_msg,
         branch_name = branch,
+        dir_path = path.split("/")[:-1],
         path = path,
     )
         
