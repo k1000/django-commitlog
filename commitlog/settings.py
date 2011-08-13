08e75eed0d48d8978113ec87d2e8971b0cf1fec1
k1000 <selwak@gmail.com>
2011-08-13 23:44:57 +0100

    multiple repo support

--- commitlog/settings.py
+++ commitlog/settings.py
@@ -1,8 +1,10 @@
-# Create your views here.
-  
+
 from django.conf import settings
 
-REPO_DIR = getattr(settings, "REPO_DIR", "")
+REPOS = getattr(settings, "REPOS", {
+	"local":"",
+})
+
 REPO_BRANCH = getattr(settings, "REPO_BRANCH", "master")
 REPO_RESTRICT_VIEW = getattr(settings, "REPO_RESTRICT_VIEW", True)
 REPO_ITEMS_IN_PAGE = getattr(settings, "REPO_VIEW_IN_PAGE", 10)
--- commitlog/urls.py
+++ commitlog/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls.defaults import *
 
 # place app url patterns here
 urlpatterns = patterns('',
-	url(r'^(?P<branch>[a-z0-9\-_]+)/edit/(?P<path>[a-zA-Z0-9\-_\.\/]+)$', "commitlog.views.edit_file", name='commitlog-edit-file'),
-    url(r'^(?P<branch>[a-z0-9\-_]+)/tree/(?P<path>[a-zA-Z0-9\-_\.\/]*)$', "commitlog.views.tree_view", name='commitlog-tree-view'),
-    url(r'^(?P<branch>[a-z0-9\-_]+)/$', "commitlog.views.log_view", name='commitlog-branch'),
+	url(r'^(?P<repo_name>[a-z0-9\-_]+)/(?P<branch>[a-z0-9\-_]+)/edit/(?P<path>[a-zA-Z0-9\-_\.\/]+)$', "commitlog.views.edit_file", name='commitlog-edit-file'),
+    url(r'^(?P<repo_name>[a-z0-9\-_]+)/(?P<branch>[a-z0-9\-_]+)/tree/(?P<path>[a-zA-Z0-9\-_\.\/]*)$', "commitlog.views.tree_view", name='commitlog-tree-view'),
+    url(r'^(?P<repo_name>[a-z0-9\-_]+)/(?P<branch>[a-z0-9\-_]+)/$', "commitlog.views.log_view", name='commitlog-branch'),
+    url(r'^(?P<repo_name>[a-z0-9\-_]+)/$', "commitlog.views.branches_view", name='commitlog-branches'),
+    url(r'^/$', "commitlog.views.repos_view", name='commitlog-repos'),
 )
